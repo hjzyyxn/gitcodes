e1dacd95ada5666924114a433fb4ca31c4b6d12d
hjzyyxn <hjzyyxn@126.com>
2017-08-02 17:11:36 +0800

    List1

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -94,5 +94,38 @@ public class MyLinkedList<AnyType> implements Iterable<AnyType> {
 	private class LinkedListIterator implements java.util.Iterator<AnyType> {
 		private Node<AnyType> current = beginMarker.next;
 		private int expectedModCount = modCount;
+		private boolean okToRemove = false;
+
+		public boolean hasNext() {
+			return current != endMarker;
+		}
+
+		public AnyType next() {
+			if(modCount != expectedModCount)
+				throw new java.util.ConcurrentModificationException();
+			if(!hasNext())
+				throw new java.util.NoSuchElementException();
+
+			AnyType nextItem = current.data;
+			current = current.next;
+			okToRemove = true;
+			return nextItem;
+		}
+
+		public void remove() {
+			if(modCount != expectedModCount)
+				throw new java.util.ConcurrentModificationException();
+			if(!okToRemove)
+				throw new IllegalStateException();
+
+			MyLinkedList.this.remove(current.prev);
+			okToRemove = false;
+			expectedModCount++;
+		}
 	}
+
+	private int theSize;
+	private int modCount = 0;
+	private Node<AnyType> beginMarker;
+	private Node<AnyType> endMarker;
 }

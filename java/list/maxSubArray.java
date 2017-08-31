54bf5b8b113e48514a5df3ffe89fc72d61fb6e28
hjzyyxn <hjzyyxn@126.com>
2017-08-31 15:53:59 +0800

    AVL

--- java/list/maxSubArray.java
+++ java/list/maxSubArray.java
@@ -1,14 +1,12 @@
-public class Solution {
-    public int maxSubArray(int[] A) {
-        int max = Integer.MIN_VALUE, sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (sum < 0) 
-                sum = A[i];
-            else 
-                sum += A[i];
-            if (sum > max)
-                max = sum;
-        }
-        return max;
+public int maxSubArray(int[] A) {
+    int max = Integer.MIN_VALUE, sum = 0;
+    for (int i = 0; i < A.length; i++) {
+        if (sum < 0) 
+            sum = A[i];
+        else 
+            sum += A[i];
+        if (sum > max)
+            max = sum;
     }
+    return max;
 }

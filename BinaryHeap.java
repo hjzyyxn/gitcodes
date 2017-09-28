362e92eddb1a9ea0171bded371a72b33184ea94f
hjzyyxn <hjzyyxn@126.com>
2017-09-28 14:28:16 +0800

    heap

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -1,8 +1,21 @@
 //BinaryHeap to build ADT
 public class BinaryHeap<AnyType extends Comparable<? super AnyType>> {
-	public BinaryHeap() {}
-	public BinaryHeap(int capacity) {}
-	public BinaryHeap(AnyType[] items) {}
+	public BinaryHeap() {
+		this( DEFAULT_CAPACITY );
+	}
+	public BinaryHeap(int capacity) {
+		currentSize = 0;
+        array = (AnyType[]) new Comparable[ capacity + 1 ];
+	}
+	public BinaryHeap(AnyType[] items) {
+		currentSize = items.length;
+		array = (AnyType[]) new Comparable[(currentSize + 2) * 11 / 10];
+
+		int i = 1;
+		for(AnyType item : items)
+			array[i++] = item;
+		buildHeap();
+	}
 
 	public void insert(AnyType x) {
 		if(currentSize == array.length - 1)
@@ -13,18 +26,58 @@ public class BinaryHeap<AnyType extends Comparable<? super AnyType>> {
 			array[hole] = array[hole/2];
 		array[hole] = x;
 	}
-	public AnyType findMin() {}
-	public AnyType deleteMin() {}
-	public boolean isEmpty() {}
-	public void makeEmpty() {}
+	public AnyType findMin() {
+		if( isEmpty( ) )
+            throw new UnderflowException( );
+        return array[1];
+	}
+	public AnyType deleteMin() {
+		if(isEmpty())
+			throw new UnderflowException();
+
+		AnyType minItem = findMin();
+		array[1] = array[currentSize--];
+		percolateDown(1);
+
+		return minItem;
+	}
+	public boolean isEmpty() {
+		return currentSize == 0;
+	}
+	public void makeEmpty() {
+		currentSize = 0;
+	}
 
 	private static final int DEFAULT_CAPACITY = 10;
 
 	private int currentSize;
 	private AnyType[] array;
 
-	private void percolateDown(int hole) {}
-	private void buildHeap() {}
-	private void enlargeArray(int newSize) {}
+	private void percolateDown(int hole) {
+		int child;
+		AnyType tmp = array[hole];
+
+		for(; hole * 2 <= currentSize; hole = child) {
+			child = hole * 2;
+			if(child != currentSize &&
+				array[child + 1].compareTo(array[child]) < 0)
+				child++;
+			if(array[child].compareTo(tmp) < 0)
+				array[hole] = array[child];
+			else
+				break;
+		}
+		array[hole] = tmp;
+	}
+	private void buildHeap() {
+		for(int i = currentSize / 2; i > 0; i--)
+			percolateDown(i);
+	}
+	private void enlargeArray(int newSize) {
+		AnyType [] old = array;
+            array = (AnyType []) new Comparable[ newSize ];
+            for( int i = 0; i < old.length; i++ )
+                array[ i ] = old[ i ]; 
+	}
 
 }

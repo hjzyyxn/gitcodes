c755f3a9650a5d201445bf52ca2c6861f57ccfb6
hjzyyxn <hjzyyxn@126.com>
2018-12-10 15:50:22 +0800

    12.10

---
+++ java/DesignPattern/src/observer/DigitObserver.java
@@ -0,0 +1,17 @@
+package observer;
+
+public class DigitObserver implements Observer {
+
+	@Override
+	public void update(NumberGenerator generator) {
+		// TODO Auto-generated method stub
+		System.out.println("DigitObserver:" + generator.getNumber());
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			
+		}
+
+	}
+
+}

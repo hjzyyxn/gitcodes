8111cf5c8f607b547fa97e43b9dfbe9fdaaac1b1
hjzyyxn <hjzyyxn@126.com>
2018-12-15 21:06:39 +0800

    12.15

---
+++ java/DesignPattern/src/command/DrawCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+import java.awt.Point;
+
+public class DrawCommand implements Command {
+	protected Drawable drawable;
+	private Point position;
+	
+	public DrawCommand(Drawable drawable, Point position) {
+		this.drawable = drawable;
+		this.position = position;
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		drawable.draw(position.x, position.y);
+
+	}
+
+}
---
+++ java/DesignPattern/src/command/Main.java
@@ -0,0 +1,109 @@
+package command;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main extends JFrame implements ActionListener, MouseMotionListener, WindowListener {
+	private MacroCommand history = new MacroCommand();
+	private DrawCanvas canvas = new DrawCanvas(400, 400, history);
+	private JButton clearButton = new JButton("clear");
+	
+	public Main(String title) {
+		super(title);
+		this.addWindowListener(this);
+		canvas.addMouseMotionListener(this);
+		clearButton.addActionListener(this);
+		
+		Box buttonBox = new Box(BoxLayout.X_AXIS);
+		buttonBox.add(clearButton);
+		Box mainBox = new Box(BoxLayout.Y_AXIS);
+		mainBox.add(buttonBox);
+		mainBox.add(canvas);
+		getContentPane().add(mainBox);
+		
+		pack();
+		show();
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		Command cmd = new DrawCommand(canvas, e.getPoint());
+		history.append(cmd);
+		cmd.execute();
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == clearButton) {
+			history.clear();
+			canvas.repaint();
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		new Main("Command Pattern Sample");
+	}
+
+}

fb55157a9486b2dc25533152a91ddb705ecc8c2d
hjzyyxn <hjzyyxn@126.com>
2018-12-25 23:08:39 +0800

    12.25

---
+++ springmvc/annotated1/src/main/java/controller/ProductController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.math.BigDecimal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import domain.Product;
+import form.ProductForm;
+
+@Controller
+public class ProductController {
+	private static final Log logger = LogFactory
+			.getLog(ProductController.class);
+	
+	@RequestMapping(value="/input-product")
+	public String inputProduct() {
+		logger.info("inputProduct called");
+		return "ProductForm";
+	}
+	
+	@RequestMapping(value="/save-product")
+	public String saveProduct(ProductForm productForm, Model model) {
+		logger.info("saveProduct called");
+		
+		Product product = new Product();
+		product.setName(productForm.getName());
+		product.setDescription(productForm.getDescription());
+		try {
+			product.setPrice(new BigDecimal(productForm.getPrice()));
+		} catch (NumberFormatException e) {
+			
+		}
+		
+		model.addAttribute("product", product);
+		return "ProductDetails";
+	}
+
+}

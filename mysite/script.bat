2c03b5a2654619c8bda2b2eea9af3301865957c8
hjzyyxn <hjzyyxn@126.com>
2020-05-10 23:44:20 +0900

    delete mysite

--- mysite/script.bat
+++
@@ -1,3 +0,0 @@
-cd F:\stanford-ner-2014-01-04\stanford-ner-2014-01-04
-
-java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.muc.7class.distsim.crf.ser.gz -port 8080 -outputFormat inlineXML

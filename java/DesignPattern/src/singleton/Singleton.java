08c090d8bae7c2798b814b3d7bee3c61fe78129d
hjzyyxn <hjzyyxn@126.com>
2020-08-02 15:57:32 +0800

    update singleton

--- java/DesignPattern/src/singleton/Singleton.java
+++ java/DesignPattern/src/singleton/Singleton.java
@@ -9,3 +9,30 @@ public class Singleton {
 		return singleton;
 	}
 }
+
+/*public class Singleton {
+	private static volatile Singleton singleton;
+	private Singleton() {}
+	public static Singleton getInstance() {
+		if (singleton == null) {
+			synchronized (Singleton.class) {
+				if (singleton == null) {
+					singleton = new Singleton();
+				}
+			}
+		}
+		return singleton;
+	}
+}*/
+
+/*public class Singleton {
+	private Singleton() {}
+	
+	private static class SingletonInstance {
+		private static final Singleton INSTANCE = new Singleton();
+	}
+	
+	public static Singleton getInstance() {
+		return SingletonInstance.INSTANCE;
+	}
+}*/
